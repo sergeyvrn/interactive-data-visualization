056e033c29fa9adb4ad30091212bf1e49609403d
Sergey Voronov <sergey.vrn@gmail.com>
2016-05-22 22:37:04 +0300

    Chapter 9 data with keys

--- chapter9.js
+++ chapter9.js
@@ -1,7 +1,27 @@
 d3 = d3 || {};
 
-var dataSet = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
-    11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
+var dataSet = [
+    {key: 0, value: 5},
+    {key: 1, value: 10},
+    {key: 2, value: 13},
+    {key: 3, value: 19},
+    {key: 4, value: 21},
+    {key: 5, value: 25},
+    {key: 6, value: 22},
+    {key: 7, value: 18},
+    {key: 8, value: 15},
+    {key: 9, value: 13},
+    {key: 10, value: 11},
+    {key: 11, value: 12},
+    {key: 12, value: 15},
+    {key: 13, value: 20},
+    {key: 14, value: 18},
+    {key: 15, value: 17},
+    {key: 16, value: 16},
+    {key: 17, value: 18},
+    {key: 18, value: 23},
+    {key: 19, value: 25}
+];
 
 const w = 600;
 const h = 250;
@@ -12,11 +32,11 @@ const xScale = d3.scale.ordinal()
     .rangeRoundBands([0, w], 0.05);
 
 const yScale = d3.scale.linear()
-    .domain([0, d3.max(dataSet)])
+    .domain([0, d3.max(dataSet, d => d.value)])
     .range([0, h]);
 
 const colorScale = d3.scale.linear()
-    .domain([0, d3.max(dataSet)])
+    .domain([0, d3.max(dataSet, d => d.value)])
     .rangeRound([0, 255]);
 
 const svg = d3.select("body")
@@ -29,19 +49,19 @@ svg.selectAll("rect")
     .append("rect")
     .attr({
         x: (d, i) => xScale(i),
-        y: d => h - yScale(d),
+        y: d => h - yScale(d.value),
         width: xScale.rangeBand(),
-        height: d => yScale(d),
-        fill: d => "rgb(0, 0, " + (d * 10) + ")"
+        height: d => yScale(d.value),
+        fill: d => "rgb(0, 0, " + (d.value * 10) + ")"
     });
 svg.selectAll("text")
     .data(dataSet)
     .enter()
     .append("text")
-    .text(d => d)
+    .text(d => d.value)
     .attr({
         x: (d, i) => xScale(i) + xScale.rangeBand() / 2,
-        y: d => h - yScale(d) + 14,
+        y: d => h - yScale(d.value) + 14,
         "font-family": "sans-serif",
         "font-size": "11px",
         fill: "white",
@@ -50,15 +70,18 @@ svg.selectAll("text")
 
 const maxValue = 25;
 function newNumber() {
-    return Math.floor(Math.random() * maxValue);
+    return {
+        key: d3.max(dataSet, d => d.key) + 1,
+        value: Math.floor(Math.random() * maxValue)
+    }
 }
 
 d3.select("#update")
     .on("click", () => {
         dataSet = dataSet.map(newNumber);
 
-        yScale.domain([0, d3.max(dataSet)]);
-        colorScale.domain([0, d3.max(dataSet)]);
+        yScale.domain([0, d3.max(dataSet, d => d.value)]);
+        colorScale.domain([0, d3.max(dataSet, d => d.value)]);
 
         svg.selectAll("rect")
             .data(dataSet)
@@ -68,10 +91,10 @@ d3.select("#update")
             .ease("linear")
             .attr({
                 x: (d, i) => xScale(i),
-                y: d => h - yScale(d),
+                y: d => h - yScale(d.value),
                 width: xScale.rangeBand(),
-                height: d => yScale(d),
-                fill: d => "rgb(0, 0, " + colorScale(d) + ")"
+                height: d => yScale(d.value),
+                fill: d => "rgb(0, 0, " + colorScale(d.value) + ")"
             });
         svg.selectAll("text")
             .data(dataSet)
@@ -81,7 +104,7 @@ d3.select("#update")
             .ease("linear")
             .attr({
                 x: (d, i) => xScale(i) + xScale.rangeBand() / 2,
-                y: d => h - yScale(d) + 14
+                y: d => h - yScale(d.value) + 14
             });
     });
 
@@ -90,17 +113,17 @@ d3.select("#add")
     .on("click", () => {
         dataSet.push(newNumber());
         xScale.domain(d3.range((dataSet.length)));
-        yScale.domain([0, d3.max(dataSet)]);
+        yScale.domain([0, d3.max(dataSet, d => d.value)]);
 
         const bars = svg.selectAll("rect").data(dataSet);
         bars.enter()
             .append("rect")
             .attr({
                 x: w,
-                y: d => h - yScale(d),
+                y: d => h - yScale(d.value),
                 width: xScale.rangeBand(),
-                height: d => yScale(d),
-                fill: d => "rgb(0, 0, " + colorScale(d) + ")"
+                height: d => yScale(d.value),
+                fill: d => "rgb(0, 0, " + colorScale(d.value) + ")"
             });
         bars.transition().duration(addDuration)
             .attr({
@@ -109,9 +132,9 @@ d3.select("#add")
             });
 
         const labels = svg.selectAll("text").data(dataSet);
-        labels.enter().append("text").text(d => d).attr({
+        labels.enter().append("text").text(d => d.value).attr({
             x: w + xScale.rangeBand() / 2,
-            y: d => h - yScale(d) + 14,
+            y: d => h - yScale(d.value) + 14,
             "font-family": "sans-serif",
             "font-size": "11px",
             fill: "white",
@@ -119,7 +142,7 @@ d3.select("#add")
         });
         labels.transition().duration(addDuration).attr({
             x: (d, i) => xScale(i) + xScale.rangeBand() / 2,
-            y: d => h - yScale(d) + 14
+            y: d => h - yScale(d.value) + 14
         })
     });
 
